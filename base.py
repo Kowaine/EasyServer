c6b87a77a2b837ca1e130550c3c2c9af46fd26ff
Kowaine <kowaine@qq.com>
2021-01-04 03:56:10 +0800

    一些细节修正

--- base.py
+++ base.py
@@ -2,7 +2,7 @@
 @Author: Kowaine
 @Description: 一些基础的数据结构和类型
 @Date: 2021-01-03 22:25:39
-@LastEditTime: 2021-01-04 03:21:39
+@LastEditTime: 2021-01-04 03:47:00
 """
 from gevent import socket, monkey
 import sys
@@ -50,7 +50,7 @@ class BaseServer():
         接收到连接时处理
         @args:
             connection 连接socket :socket
-            addr 发来连接的地址 :string
+            addr 发来连接的地址 :tuple
         """
         request = self.preprocess_request(connection, addr)
         response = self.process_request(request)
@@ -90,6 +90,7 @@ class BaseServer():
         """
         try:
             sys.stdout.write(start_msg)
+            sys.stdout.flush()
             self.run()
         except KeyboardInterrupt:
             sys.exit(0)
